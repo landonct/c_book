9be06f03cc516e6d819aa5c564e6c118b1c23359
landonct <120686608+landonct@users.noreply.github.com>
2026-02-25 23:36:22 -0500

    solve chaper 1 exercise 1 and 2

--- counting.c
+++ counting.c
@@ -1,15 +1,35 @@
 #include <stdio.h>
 
-int main() {
-    int count = 0, nc;
+int main()
+{
+    // int count = 0, nc;
 
-    /*Count tabs, newline, blanks*/
-    while((nc = getchar()) != EOF) {
-        if(nc == '\n' || nc == '\t' || nc == ' ') {
-            ++count;
+    // /*Count tabs, newline, blanks*/
+    // while((nc = getchar()) != EOF) {
+    //     if(nc == '\n' || nc == '\t' || nc == ' ') {
+    //         ++count;
+    //     }
+    // }
+    // printf("Count: %d\n", count);
+    // return 0;
+
+    /*Copy input to output and replace all blanks with only 1*/
+    int nc;
+    int was_space = 0;
+    while ((nc = getchar()) != EOF)
+    {
+        if (nc != ' ')
+        {
+            putchar(nc);
+            if (was_space)
+            {
+                was_space = 0;
+            }
+        }
+        else if (nc == ' ' && !was_space)
+        {
+            putchar(nc);
+            was_space = 1;
         }
     }
-    printf("Count: %d\n", count);
-    return 0;
-    /**/
 }
