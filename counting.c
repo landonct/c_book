53495c9831517fa17d445e4bc612dd233959209a
Landon Thomas <120686608+landonct@users.noreply.github.com>
2026-02-26 15:37:30 -0500

    Refactor counting.c and clean up commented code

--- counting.c
+++ counting.c
@@ -2,9 +2,8 @@
 
 int main()
 {
+    /*Count tabs, newline, blanks*/
     // int count = 0, nc;
-
-    // /*Count tabs, newline, blanks*/
     // while((nc = getchar()) != EOF) {
     //     if(nc == '\n' || nc == '\t' || nc == ' ') {
     //         ++count;
@@ -14,22 +13,101 @@ int main()
     // return 0;
 
     /*Copy input to output and replace all blanks with only 1*/
-    int nc;
-    int was_space = 0;
-    while ((nc = getchar()) != EOF)
+    // int nc;
+    // int was_space = 0;
+    // while ((nc = getchar()) != EOF)
+    // {
+    //     if (nc != ' ')
+    //     {
+    //         putchar(nc);
+    //         if (was_space)
+    //         {
+    //             was_space = 0;
+    //         }
+    //     }
+    //     else if (nc == ' ' && !was_space)
+    //     {
+    //         putchar(nc);
+    //         was_space = 1;
+    //     }
+    // }
+
+    /*Make tabs, backspace, and \ visible in output*/
+    // int nc;
+    // while((nc = getchar()) != EOF)
+    // {
+    //     if(nc == '\t') 
+    //     {
+    //         putchar('\\');
+    //         putchar('t');
+    //     } else if(nc == '\b')
+    //     {
+    //         putchar('\\');
+    //         putchar('b');
+    //     } else if(nc == '\\')
+    //     {
+    //         putchar('\\');
+    //         putchar('\\');
+    //     } else
+    //     {
+    //         putchar(nc);
+    //     }
+    // }
+
+    /*Print input with one word on each line*/
+    // int c;
+    // while((c = getchar()) != EOF)
+    // {
+    //     if(c == ' ' || c == '\t')
+    //     {
+    //         putchar('\n');
+    //     } else
+    //     {
+    //         putchar(c);
+    //     }
+    // }
+
+    /*Print histogram of word lengths (horz then vertical)*/
+    int bars[10] = {0}, c, nc = 0;
+    while((c = getchar()) != EOF)
     {
-        if (nc != ' ')
+        if(c != ' ' && c != '\n')
         {
-            putchar(nc);
-            if (was_space)
+            nc++;
+        } else if(c == ' ')
+        {
+            if(nc >= 10)
             {
-                was_space = 0;
+                bars[9]++;
+            } else
+            {
+                bars[nc]++;
+            }
+            nc = 0;
+        } else {
+            if(nc >= 10)
+            {
+              bars[9]++;
+            } else
+            {
+                bars[nc]++;
             }
         }
-        else if (nc == ' ' && !was_space)
-        {
-            putchar(nc);
-            was_space = 1;
-        }
     }
+
+    for(int i = 0; i < 10; i++)
+    {
+        printf("%d | ", i);
+        for(int j = 0; j < bars[i]; j++)
+        {
+            printf("=");
+        }
+        if(bars[i] > 0)
+            printf(" %d", bars[i]);
+        printf("\n");
+    }
+
+    /*Make histogram of different frequencies of input*/
+
+    return 0;
 }
